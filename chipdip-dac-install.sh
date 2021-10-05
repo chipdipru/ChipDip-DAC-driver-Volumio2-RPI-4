ad63fcbd97b90498a7fde0546560f0f19a76084f
chipdip <43340836+chipdipru@users.noreply.github.com>
2021-10-05 22:58:41 +0300

    Update chipdip-dac-install.sh

--- chipdip-dac-install.sh
+++ chipdip-dac-install.sh
@@ -2,12 +2,10 @@
 
 echo "Installing ChipDip DACs..."
 
-cp /volumio/ChipDipDACs/chipdip-i2s-master-dac.dtbo /boot/overlays/chipdip-i2s-master-dac.dtbo
-cp /volumio/ChipDipDACs/./RasGPIOCtrl_v5 /volumio/app/plugins/system_controller/i2s_dacs/scripts/./RasGPIOCtrl_v5
-cp /volumio/ChipDipDACs/chipdip-dac-init.sh /volumio/app/plugins/system_controller/i2s_dacs/scripts/chipdip-dac-init.sh
-chmod +x /volumio/app/plugins/system_controller/i2s_dacs/scripts/./RasGPIOCtrl_v5
-chmod +x /volumio/app/plugins/system_controller/i2s_dacs/scripts/chipdip-dac-init.sh
-sed -i '4 a {"id":"chipdip-master-dac","name":"CHIPDIP Master DAC","overlay":"chipdip-i2s-master-dac","alsanum":"2","mixer":"","modules":"","script":"chipdip-dac-init.sh","needsreboot":"yes"},' /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
-sed -i '5 a {"id":"chipdip-slave-dac","name":"CHIPDIP Slave DAC","overlay":"hifiberry-dac","alsanum":"2","mixer":"","modules":"","script":"chipdip-dac-init.sh","needsreboot":"yes"},' /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
+sudo cp ChipDip-DAC-driver-Volumio-RPI4/snd-soc-chipdip-dac-4.19.188-v71+.ko /lib/modules/`uname -r`/kernel/sound/soc/bcm/snd-soc-chipdip-dac.ko
+sudo cp ChipDip-DAC-driver-Volumio-RPI4/chipdip-dac.dtbo /boot/overlays/chipdip-dac.dtbo
+sudo depmod
+
+sed -i '4 a {"id":"chipdip-master-dac","name":"ChipDip DAC","overlay":"chipdip-dac","alsanum":"2","mixer":"","modules":"","script":"","needsreboot":"yes"},' /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
 
 echo "Reboot to complete installation"
